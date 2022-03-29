cd09f6c2385ca29264dc97abc0d085cdd1eba6f5
muccidavide <davidemucci0804@gmail.com>
2022-03-29 16:18:22 +0200

    Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,5 @@
-/* 
-Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
-stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte?
- Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-non bisogna ricordarsi a memoria come generare dei numeri random, c'é W3S per questo https://www.w3schools.com/js/js_random.asp
-BONUS:
-Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
-*/
+
+// dichiaro costanti per elenco email autorizzate e quell ainserita dall'utente
 
 const usersAuthorizedEmails = ["aldo@libero.it", "giovvani@gmail.com", "giacomo@gmail.com"]
 const userEmailPrompt = prompt("Inserisci qui la tua email")
@@ -33,7 +16,6 @@ const userEmailPrompt = prompt("Inserisci qui la tua email")
 
   } 
 
-  
 ///// !!!!! HERE BONUS  !!!!! //////
 
 const userEmailInput = document.getElementById('emailId');
@@ -48,11 +30,11 @@ function () {
         const usersAuthorizedEmail = usersAuthorizedEmails[i];
         if(userEmailInput.value == usersAuthorizedEmail){
             resultAccess.insertAdjacentHTML("beforeend", 
-            "Utente Autorizzato"
+            "<span>Utente Autorizzato</span>"
             )
         } else {
             resultAccess.insertAdjacentHTML("beforeend", 
-            "Utente Non Autorizzato"
+            "<span>Utente Non Autorizzato</span>"
             )
         }break 
     
@@ -60,19 +42,17 @@ function () {
 }
 )
    
+  /////////////////// Gioco dei Dadi ///////////////////////////////////
 
-
-
-  // Gioco dei Dadi 
   // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
  const userNumberRnd = parseInt(Math.random()*6) + 1;
  const computerNumberRnd = parseInt(Math.random()*6) + 1;
 
  // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
- if (userNumberRnd < computerNumberRnd) {
+ if (userNumberRnd < computerNumberRnd){
      console.log("Vince il Pc");
  } else if(userNumberRnd === computerNumberRnd){
-     console.log("Patta");
+     console.log("Pareggio");
  } else {
     console.log("Bravo hai vinto!"); }
